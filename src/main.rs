013bb1c563a49757ec73a84f21234bc12adf8b3c
Hyijun <1823144292@qq.com>
2019-07-30 18:28:36 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,26 @@ use std::io;
 fn main() {
     let rea:Regex = Regex::new("<li><a title=\"(?:.*?)\" href=\"(.*?)\" >(?:.*?)</a>(?:.*?)</li>").unwrap();
     let reb:Regex = Regex::new("<div class=\"cartoon_online_border\" >(?:.*?)<ul>(?:.*?)(.*?)</ul>").unwrap();
-    let mut html = reqwest::get("https://manhua.dmzj.com/biedangounijiangle/").unwrap();
-    let mut f = File::create("F:\\test\\a.html").unwrap();
     let mut str_1 = String::new();
-    html.read_to_string(& mut str_1);
+    str_1 = get_html("https://manhua.dmzj.com/biedangounijiangle/");
+    let mut f = File::create("F:\\test\\a.html").unwrap();
+    let mut str_2 = String::new();
+    let mut urls:Vec<String>= Vec::new();
     str_1 = str_1.replace("\n", "");
-//    println!("{}", str_1);
+
     for each in reb.captures_iter(&str_1) {
-        println!("{}", each.get(0).unwrap().as_str());
+        str_2 = each.get(0).unwrap().as_str().to_string();
     }
-    println!("{:?}", reb.find(&str_1));
-//    let mut buffer = [u8];
-//    println!("{:?}", html.read(buffer));
 
+    for each in rea.captures_iter(&str_2){
+        urls.push("https://manhua.dmzj.com/".to_string() + each.get(1).unwrap().as_str());
+    }
+//    println!("{:?}", urls);
+}
 
+fn get_html(url: &str) -> String{
+    let mut html = reqwest::get(url).unwrap();
+    let mut string = String::new();
+    html.read_to_string(& mut string);
+    string
 }
